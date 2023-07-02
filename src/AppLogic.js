54be2ca4ca2a39a625d0c7c44b9f02dbe53ada35
Ciocoiu Radu-Mihail <ciocoiuradu653@gmail.com>
2023-07-02 19:18:59 +0300

    commit changes

--- src/AppLogic.js
+++ src/AppLogic.js
@@ -2,21 +2,6 @@ import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
 
-// Import SVGs style & SVGs for different weather conditions
-
-import { ReactComponent as ClearDaySVG } from './clear-day.svg';
-import { ReactComponent as ClearNightSVG } from './clear-night.svg';
-import { ReactComponent as CloudySVG } from './cloudy.svg';
-import { ReactComponent as DrizzleSVG } from './drizzle.svg';
-import { ReactComponent as FogDaySVG } from './fog-day.svg';
-import { ReactComponent as FogNightSVG } from './fog-night.svg';
-import { ReactComponent as FogSVG } from './fog.svg';
-import { ReactComponent as NotAvailableSVG } from './not-available.svg';
-import { ReactComponent as RainSVG } from './rain.svg';
-import { ReactComponent as SnowSVG } from './snow.svg';
-import { ReactComponent as WindSVG } from './wind.svg';
-
-
 // Firebase configuration
 const firebaseConfig = {
   // Firebase API configuration details
@@ -58,59 +43,6 @@ function AppLogic({ station }) {
     };
   }, [station]);
 
-  // Function to determine the weather condition based on the weather data
-  const getWeatherCondition = () => {
-    const { temperature, pressure, humidity, rainfall_intensity, wind_speed } = weatherData;
-
-    if (rainfall_intensity === 0) {
-      return 'not-available';
-    } else if (humidity === 100 && temperature < 10) {
-      return 'fog';
-    } else if (humidity === 100) {
-      return 'fog';
-    } else if (humidity > 60 && rainfall_intensity > 0) {
-      return 'rain';
-    } else if (rainfall_intensity > 0 && rainfall_intensity <= 2) {
-      return 'drizzle';
-    } else if (rainfall_intensity > 2 && rainfall_intensity <= 10) {
-      return 'rain';
-    } else if (temperature < 0) {
-      return 'snow';
-    } else if (temperature >= 0 && temperature <= 20) {
-      return 'clear-day';
-    } else if (temperature > 20) {
-      return 'clear-night';
-    } else {
-      return 'cloudy';
-    }
-  };
-
-  // Function to render the corresponding SVG based on the weather condition
-  const renderWeatherSVG = () => {
-    const weatherCondition = getWeatherCondition();
-
-    switch (weatherCondition) {
-      case 'clear-day':
-        return <ClearDaySVG />;
-      case 'clear-night':
-        return <ClearNightSVG />;
-      case 'cloudy':
-        return <CloudySVG />;
-      case 'drizzle':
-        return <DrizzleSVG />;
-      case 'fog':
-        return <FogSVG />;
-      case 'not-available':
-        return <NotAvailableSVG />;
-      case 'rain':
-        return <RainSVG />;
-      case 'snow':
-        return <SnowSVG />;
-      default:
-        return <WindSVG />;
-    }
-  };
-
   return (
     <div className="elemContainer">
       {weatherData && (
@@ -138,9 +70,6 @@ function AppLogic({ station }) {
               TIMESTAMP: {weatherData.timestamp.day}/{weatherData.timestamp.month}/{weatherData.timestamp.year}{' '}
               {weatherData.timestamp.hour}:{weatherData.timestamp.minute}:{weatherData.timestamp.second}
             </p>
-          </div>
-          <div className="row">
-            <div className="weather-svg">{renderWeatherSVG()}</div>
           </div>
         </>
       )}
